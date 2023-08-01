bc3e552beb801c73c7e0d7f55abece49e5ea02c7
yousef langaran <youseflangaran65@gmail.com>
2023-08-01 08:43:42 +0330

    change gitignore file

--- src/CSharp/Blazor/Program.cs
+++ src/CSharp/Blazor/Program.cs
@@ -1,20 +1,30 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 <<<<<<< HEAD
+<<<<<<< HEAD
 using MudBlazor.Services;
 
 =======
 >>>>>>> 05d4eea (Creating new project of support)
+=======
+using MudBlazor.Services;
+
+>>>>>>> 2e16d2b (change gitignore file)
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 builder.Services.AddMudServices();
 
 =======
 >>>>>>> 05d4eea (Creating new project of support)
+=======
+builder.Services.AddMudServices();
+
+>>>>>>> 2e16d2b (change gitignore file)
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
