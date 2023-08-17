e8ddba406002f68901dcf6fc2a9eb9f386c5aff8
a.nemati <Aa@09397829464>
2023-08-17 10:56:09 +0330

    Initialize project

---
+++ src/Dart/Flutter/lib/ticketing.dart
@@ -0,0 +1,15 @@
+library ticketing;
+import 'package:flutter/material.dart';
+
+class TicketingPage extends StatelessWidget {
+  TicketingPage({
+    required this.link,
+    required this.addLink
+  });
+  final String link;
+  final String addLink;
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
